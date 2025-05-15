90984bb3f7fae83134458a596513ee6190f8e35e
Talha Nalbant <nalbanttalha75@gmail.com>
2025-05-16 00:59:23 +0300

    i changed the add_product.php

--- add_product.php
+++ add_product.php
@@ -2,9 +2,9 @@
 require 'db.php';
 session_start();
 // DEV_MODE girişi var, role kontrolü
-if($_SESSION['user']['type']!=='market') exit('Yetkisiz');
+if ($_SESSION['user']['type'] !== 'market') exit('Yetkisiz');
 // Post geldiyse işle
-if($_SERVER['REQUEST_METHOD']==='POST'){
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   // filtrasyon vs.
   $title = $_POST['title'];
   $stock = (int)$_POST['stock'];
@@ -15,15 +15,16 @@ if($_SERVER['REQUEST_METHOD']==='POST'){
   $stmt = $db->prepare("INSERT INTO products
     (market_id,title,stock,normal_price,discounted_price,expiration_date)
     VALUES (?,?,?,?,?,?)");
-  $stmt->execute([$_SESSION['user_id'],$title,$stock,$norm,$disc,$exp]);
+  $stmt->execute([$_SESSION['user']['id'], $title, $stock, $norm, $disc, $exp]);
   header('Location: dashboard.php');
   exit;
 }
 ?>
 <form method="POST">
-  Başlık: <input name="title"><br>
-  Stok:   <input name="stock" type="number"><br>
-  Fiyat:  <input name="discounted_price" type="number" step="0.01"><br>
-  Bitiş:  <input name="expiration_date" type="date"><br>
-  <button>Ekle</button>
+  Name: <input name="title"><br>
+  stock: <input name="stock" type="number"><br>
+  Price: <input name="normal_price" type="number" ><br>
+  Discounted Price: <input name="discounted_price" type="number"><br>
+  Expiration Date: <input name="expiration_date" type="date"><br>
+  <button>Add</button>
 </form>
