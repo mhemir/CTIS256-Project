cd585756ed3f3ce4d39f57ab3f01669d89dfb4ba
MikhailMushkarenko <mushkarenko777@gmail.com>
2025-05-17 16:41:34 +0300

    Update index.php

--- index.php
+++ index.php
@@ -38,7 +38,6 @@ if (isset($_COOKIE["access_token"])) {
     </div>
 </div>
 
-<!-- Bootstrap 5.3.6 JS -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO"
         crossorigin="anonymous"></script>
